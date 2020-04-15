719cdd5a287ef9d036c34f5ed0912935fd96a337
Tim Smith <code@abznak.com>
2020-04-15 14:52:07 +1000

    add test_percentage_of_offices_with_a_path__when_there_are_no_paths_should_return_0

--- test_calculate.py
+++ test_calculate.py
@@ -3,5 +3,14 @@ import calculate
 
 
 class Test(TestCase):
-    def test_percentage_of_offices_with_a_path(self):
-        self.fail()
+    def test_percentage_of_offices_with_a_path__when_there_are_always_paths_should_return_1(self):
+        office_generator = range(2)
+        office_has_path_fn = lambda x: True
+        got = calculate.percentage_of_offices_with_a_path(office_generator, office_has_path_fn)
+        self.assertEqual(1, got)
+
+    def test_percentage_of_offices_with_a_path__when_there_are_no_paths_should_return_0(self):
+        office_generator = range(2)
+        office_has_path_fn = lambda x: False
+        got = calculate.percentage_of_offices_with_a_path(office_generator, office_has_path_fn)
+        self.assertEqual(0, got)
