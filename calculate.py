4f8039a9eadf849be73ac95f8ca60e193d8be130
Tim Smith <code@abznak.com>
2020-04-15 14:47:28 +1000

    create percentage_of_offices_with_a_path

---
+++ calculate.py
@@ -0,0 +1,6 @@
+    # Return the proportion of offices that had a path
+def percentage_of_offices_with_a_path(office_generator, office_has_path_fn):
+    return 1
+
+
+
---
+++ test_calculate.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+import calculate
+
+
+class Test(TestCase):
+    def test_percentage_of_offices_with_a_path(self):
+        self.fail()
